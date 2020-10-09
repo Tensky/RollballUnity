a13090b524dac9308edffe54729d90e6198ae18a
Winston Wiradi <winstonwiradip@gmail.com>
2020-10-09 19:25:57 +0700

    Project Done

---
+++ Assets/Scripts/CollectableSpawnController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSpawnController : MonoBehaviour
+{   
+    public GameObject collectables;
+    private float timeSinceLastSpawned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCollectables();
+    }
+
+    void SpawnCollectables(){
+        timeSinceLastSpawned = 0;
+        collectables.SetActive(true);
+        var randomX = Random.Range(-19, 19);
+        var randomZ = Random.Range(-19, 19);
+        collectables.transform.position = new Vector3(randomX, 0.5f, randomZ);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+        if(timeSinceLastSpawned >= 10 || !collectables.activeInHierarchy){
+            SpawnCollectables();
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour{
+    public float movementSpeed = 1f;
+    private Rigidbody rbody;
+    private float movementX;
+    private float movementY;
+    private int scoreCounter;
+    public TextMeshProUGUI scoreCounterText;
+    public GameObject winText;
+    // Start is called before the first frame update
+    void Start(){
+        rbody = GetComponent<Rigidbody>();
+        scoreCounter = 0;  
+    }
+
+    // Update is called once per frame
+    void Update(){
+    
+    }
+
+    void SetScoreCounterText(){
+        scoreCounterText.SetText("Score : " + scoreCounter.ToString());
+        if(scoreCounter >= 12){
+            winText.SetActive(true);
+        }
+    }
+
+    void OnMove(InputValue movementValue){
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y; 
+    }
+
+    private void FixedUpdate() {
+        Vector3 movementForce = new Vector3(movementX, 0f, movementY);
+        rbody.AddForce(movementForce * movementSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Collectables")){
+            other.gameObject.SetActive(false);
+            scoreCounter++;
+            SetScoreCounterText();
+        }
+    }
+}
